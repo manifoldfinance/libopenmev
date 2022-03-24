bd99ed1807c3769a49521a847c27dee544440b34
sam bacha <sam@manifoldfinance.com>
2022-03-24 00:49:25 -0700

    feat: v0.1.1 (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-
-import type { ConnectionInfo } from '@ethersproject/web'
-import type { Eip1193Bridge } from '@ethersproject/experimental'
-import type { Actions } from '@disco3/types'
-import { Connector } from '@disco3/types'
-
-
+import type { ConnectionInfo } from '@ethersproject/web';
+import type { Eip1193Bridge } from '@ethersproject/experimental';
+import type { Actions } from '@disco3/types';
+import { Connector } from '@disco3/types';
 
 export enum SupportedChainId {
   ETHEREUM = 1,
@@ -12,63 +9,68 @@ export enum SupportedChainId {
 
 export class Url extends Connector {
   /** {@inheritdoc Connector.provider} */
-  declare public provider: Eip1193Bridge | undefined
+  public declare provider: Eip1193Bridge | undefined;
 
-  private eagerConnection?: Promise<void>
-  private url: url
+  private eagerConnection?: Promise<void>;
+  private url: url;
 
   /**
    * @param url - An RPC url.
    * @param connectEagerly - A flag indicating whether connection should be initiated when the class is constructed.
    */
   constructor(actions: Actions, url: url, connectEagerly = false) {
-    super(actions)
+    super(actions);
 
     if (connectEagerly && typeof window === 'undefined') {
-      throw new Error('connectEagerly = true is invalid for SSR, instead use the activate method in a useEffect')
+      throw new Error(
+        'connectEagerly = true is invalid for SSR, instead use the activate method in a useEffect',
+      );
     }
 
-    this.url = url
+    this.url = url;
 
-    if (connectEagerly) void this.activate()
+    if (connectEagerly) void this.activate();
   }
 
   private async isomorphicInitialize() {
-    if (this.eagerConnection) return this.eagerConnection
+    if (this.eagerConnection) return this.eagerConnection;
 
     await (this.eagerConnection = Promise.all([
-      import('@ethersproject/providers').then(({ JsonRpcProvider }) => JsonRpcProvider),
-      import('@ethersproject/experimental').then(({ Eip1193Bridge }) => Eip1193Bridge),
+      import('@ethersproject/providers').then(
+        ({ JsonRpcProvider }) => JsonRpcProvider,
+      ),
+      import('@ethersproject/experimental').then(
+        ({ Eip1193Bridge }) => Eip1193Bridge,
+      ),
     ]).then(([JsonRpcProvider, Eip1193Bridge]) => {
-      const provider = new JsonRpcProvider(this.url)
-      this.provider = new Eip1193Bridge(provider.getSigner(), provider)
-    }))
+      const provider = new JsonRpcProvider(this.url);
+      this.provider = new Eip1193Bridge(provider.getSigner(), provider);
+    }));
   }
 
   /** {@inheritdoc Connector.activate} */
   public async activate(): Promise<void> {
-    if (!this.provider) this.actions.startActivation()
+    if (!this.provider) this.actions.startActivation();
 
-    await this.isomorphicInitialize()
+    await this.isomorphicInitialize();
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return this.provider!.request({ method: 'eth_chainId' })
       .then((chainId: string) => {
-        this.actions.update({ chainId: parseChainId(chainId), accounts: [] })
+        this.actions.update({ chainId: parseChainId(chainId), accounts: [] });
       })
       .catch((error: Error) => {
-        this.actions.reportError(error)
-      })
+        this.actions.reportError(error);
+      });
   }
 }
 
-type url = string | ConnectionInfo
+type url = string | ConnectionInfo;
 
 function parseChainId(chainId: string) {
-  return Number.parseInt(chainId, 16)
+  return Number.parseInt(chainId, 16);
 }
 
-  export const SUSHIGUARD_RELAY: { [chainId in SupportedChainId]?: string } = {
-    [SupportedChainId.ETHEREUM]: 'https://api.sushirelay.com/v1',
-  }
-  
+export const SUSHIGUARD_RELAY: { [chainId in SupportedChainId]?: string } = {
+  [SupportedChainId.ETHEREUM]: 'https://api.sushirelay.com/v1',
+};
